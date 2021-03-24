657dbfafa6f3833eecd2649d146f2242bf8f30a1
driverog <driverog@everis.com>
2021-03-24 13:51:53 +0100

    Version 2 -GIT

--- src/com/everis/git/AppMain.java
+++ src/com/everis/git/AppMain.java
@@ -1,5 +1,18 @@
 package com.everis.git;
-
+/**
+ * Formacion Git
+ * 
+ * @author driverog
+ *
+ */
 public class AppMain {
 
+	/**
+	 * Metodo Principal
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hola Mundo");
+	}
 }
